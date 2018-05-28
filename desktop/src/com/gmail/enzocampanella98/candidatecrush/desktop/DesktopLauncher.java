850102f35e56f2f1d7b3d2e1fe53f78eb5698fe0
Lorenzo Campanella <enzocampanella98@gmail.com>
2018-05-28 01:20:54 -0400

    set desktop width and height to game

--- desktop/src/com/gmail/enzocampanella98/candidatecrush/desktop/DesktopLauncher.java
+++ desktop/src/com/gmail/enzocampanella98/candidatecrush/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.gmail.enzocampanella98.candidatecrush.CandidateCrush;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 600;
-		config.height = 900;
+		config.width = 540;
+		config.height = (int)Math.round(config.width * ((double)CandidateCrush.V_HEIGHT / CandidateCrush.V_WIDTH));
 		new LwjglApplication(new CandidateCrush(), config);
 }	}
 
