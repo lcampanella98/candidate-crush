eceade55378ba0ff26e2e84deede23607169972a
Lorenzo Campanella <enzocampanella98@gmail.com>
2019-11-30 08:24:54 -0500

    update libgdx version

--- desktop/src/com/gmail/enzocampanella98/candidatecrush/desktop/DesktopLauncher.java
+++ desktop/src/com/gmail/enzocampanella98/candidatecrush/desktop/DesktopLauncher.java
@@ -7,8 +7,6 @@ import com.gmail.enzocampanella98.candidatecrush.CandidateCrush;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 540;
-		config.height = (int)Math.round(config.width * ((double)CandidateCrush.V_HEIGHT / CandidateCrush.V_WIDTH));
 		new LwjglApplication(new CandidateCrush(), config);
-}	}
-
+	}
+}
