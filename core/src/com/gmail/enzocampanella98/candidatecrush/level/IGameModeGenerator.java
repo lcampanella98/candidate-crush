d07c00a0e9c9958673caf0dbec4bf404f29ab12a
Lorenzo Campanella <enzocampanella98@gmail.com>
2020-02-03 21:59:11 -0500

    complete merge

--- core/src/com/gmail/enzocampanella98/candidatecrush/level/IGameModeGenerator.java
+++ core/src/com/gmail/enzocampanella98/candidatecrush/level/IGameModeGenerator.java
@@ -1,9 +1,8 @@
 package com.gmail.enzocampanella98.candidatecrush.level;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.gmail.enzocampanella98.candidatecrush.gamemode.CCGameMode;
 import com.gmail.enzocampanella98.candidatecrush.gamemode.config.GameModeConfig;
-
-import javafx.stage.Stage;
 
 public interface IGameModeGenerator {
     CCGameMode generate(Stage stage, GameModeConfig config);
